84c8eac41a09bf2f6d516227d5c985d4f0735412
Lee Jones <lee.jones@linaro.org>
2019-06-13 15:36:52 +0100

    scripts: quick-start.sh: Allow for easy kernel compilation
    Forward the force option to over-write any previously built images.
    This passes responsibility for checking on to make-image.sh when
    compiling either Grub or the kernel.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- scripts/quick-start.sh
+++ scripts/quick-start.sh
@@ -15,12 +15,11 @@ print_blue()
 
 function usage()
 {
-    print_blue "USAGE: $0 [asus-tp370ql|hp-envy-x2]lenovo-mixx-630|lenovo-yoga-c630|generic]"
+    print_blue "USAGE: $0 [[asus-tp370ql|hp-envy-x2]lenovo-mixx-630|lenovo-yoga-c630|generic]|kernel|grub|help][-f]"
     return 1
 }
 
-if [ $# -ne 1 ]; then
-    print_blue "$0 takes exactly 1 argument"
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
     usage
 fi
 
@@ -33,8 +32,11 @@ while [ $# -gt 0 ]; do
             BUILD_KERNEL=true
             ;;
         grub)
-            BUILD_KERNEL=grub
+            BUILD_GRUB=grub
             ;;
+	-f|--force)
+	    EXTRAARGS="$EXTRAARGS -f"
+	    ;;
         help|--help|-h|?)
             usage
             ;;
@@ -54,18 +56,12 @@ startup_checks()
 	print_blue "$0 should be executed from this project's root directory"
     fi
 
-    if [ $BUILD_KERNEL ]; then
-	if ls $OUTDIR/linux-*.deb > /dev/null 2>&1; then
-	    print_blue "The output directory already contains kernel debs - either relocate or delete them to continue"
+    if [ ! $BUILD_KERNEL ] && [ ! $BUILD_GRUB ]; then
+	if ls $OUTDIR/$VMNAME.img > /dev/null 2>&1; then
+	    print_blue "The output directory already contains a VM image - either relocate or delete it to continue"
 
 	    return 1
 	fi
-    fi
-
-    if ls $OUTDIR/$vm.img > /dev/null 2>&1; then
-	print_blue "The output directory already contains a VM image - either relocate or delete it to continue"
-
-	return 1
     fi
 }
 
@@ -79,14 +75,16 @@ if [ $BUILD_KERNEL ]; then
     print_blue "Building the Linux kernel (~35 mins)"
     docker run -ti --rm --name aarch64-laptops-kernel                                          \
 	   -v $PWD/isos:/isos -v $PWD/output:/output -v $PWD/scripts:/scripts -v $PWD/src:/src \
-	   aarch64-laptops-build-env:0.1 /scripts/make-image.sh --build-kernel
+	   aarch64-laptops-build-env:0.1 /scripts/make-image.sh --build-kernel ${EXTRAARGS}
+    exit 0
 fi
 
 if [ $BUILD_GRUB ]; then
     print_blue "Building Grub (~5 mins)"
     docker run -ti --rm --name aarch64-laptops-grub                                            \
 	   -v $PWD/isos:/isos -v $PWD/output:/output -v $PWD/scripts:/scripts -v $PWD/src:/src \
-	   aarch64-laptops-build-env:0.1 /scripts/make-image.sh --build-grub
+	   aarch64-laptops-build-env:0.1 /scripts/make-image.sh --build-grub ${EXTRAARGS}
+    exit 0
 fi
 
 print_blue "Build the basic SD card image - requires user input (~2 hours manual : ~5 mins prebuilt)"
@@ -94,7 +92,7 @@ print_blue " Ubuntu installed in a VM (using LibVirt)"
 docker run -ti --privileged --name aarch64-laptops-ubuntu-vm                               \
        -v $PWD/isos:/isos -v $PWD/output:/output -v $PWD/scripts:/scripts                  \
        -v $PWD/src:/src   -v $PWD/output:/var/lib/libvirt/images                           \
-       aarch64-laptops-build-env:0.1 /scripts/make-image.sh --install-ubuntu-from-prebuilt
+       aarch64-laptops-build-env:0.1 /scripts/make-image.sh --install-ubuntu-from-prebuilt ${EXTRAARGS}
 
 print_blue "Saving the aarch64-laptops-ubuntu-vm container as an image"
 docker commit aarch64-laptops-ubuntu-vm aarch64-laptops-ubuntu-vm:0.1
@@ -106,4 +104,4 @@ print_blue "Setting up VM (~2.5 hours - manual : 30 mins - prebuilt)"
 docker run -ti --rm --privileged --name aarch64-laptops-ubuntu-vm-setup                    \
        -v $PWD/isos:/isos -v $PWD/output:/output -v $PWD/scripts:/scripts                  \
        -v $PWD/src:/src   -v $PWD/output:/var/lib/libvirt/images                           \
-       aarch64-laptops-ubuntu-vm:0.1 /scripts/make-image.sh --setup-vm-from-prebuilt ${DEVICE}
+       aarch64-laptops-ubuntu-vm:0.1 /scripts/make-image.sh --setup-vm-from-prebuilt ${DEVICE} ${EXTRAARGS}
